3bb75efc35947f369a11c59bc1cd6aaf9d0eb1e5
Andy Shora <Andy.Shora@rga.com>
2013-07-29 10:43:38 +0100

    Setup npm and bower dependencies

--- setup.sh
+++ setup.sh
@@ -2,4 +2,10 @@ echo "Please enter your app name, e.g. testApp:"
 read appName
 find . -type f -name "*.js" -print0 | xargs -0 sed -i '.bak' -e "s/newappApp/$appName/g"
 find . -type f -name "*.html" -print0 | xargs -0 sed -i '.bak' -e "s/newappApp/$appName/g"
-echo  "Done."
+echo  "Replaced app name references"
+cd app
+echo  "Installing npm dependencies"
+npm i
+echo  "Installing bower dependencies"
+bower i
+echo  "Done. You can now run 'grunt server' to see your app."

e15f9275bd38d6a85954757600a541f66583e526
Andy Shora <Andy.Shora@rga.com>
2013-08-02 14:56:53 +0100

    lunchtime updates

--- app/app/scripts/app.js
+++ app/app/scripts/app.js
@@ -1,13 +1,20 @@
 'use strict';
 
 angular.module('newappApp', [])
-  .config(function ($routeProvider) {
-    $routeProvider
-      .when('/', {
-        templateUrl: 'views/main.html',
-        controller: 'MainCtrl'
-      })
-      .otherwise({
-        redirectTo: '/'
-      });
-  });
+	.config(function ($routeProvider) {
+	$routeProvider
+		.when('/', {
+			templateUrl: 'views/main.html',
+			controller: 'MainCtrl'
+		})
+		.when('/oauth_callback', {
+			// this should be replaced by a 302 redirect in response to the OAuth sign in request (Step 1)
+			templateUrl: 'views/callback.html',
+			controller: function($scope, Twitter) {
+				Twitter.redirectUser();
+			}
+		})
+		.otherwise({
+			redirectTo: '/'
+		});
+});
---
+++ app/app/scripts/services/Twitter.js
@@ -0,0 +1,75 @@
+'use strict';
+
+var app = angular.module('TwitterProvider', []);
+app.factory('Twitter', function ($rootScope, $q) {
+
+	var self = this,
+	OAuthRequestToken = '',
+	OAuthTokenSecret = '',
+	OAuthRequestVerifier = '',
+	OAuthCallbackURL = window.location.host + '/oauth_callback',
+	apiPath = 'https://api.twitter.com/',
+	userId = null,
+	screenName = null;
+
+	return {
+		// Step 1
+		obtainRequestToken: function() {
+			$http.post(apiPath + 'oauth/request_token', { oauth_callback: encodeURIComponent(OAuthCallbackURL) })
+				.success(function(data) {
+					if (data.oauth_callback_confirmed) {
+						// success
+						OAuthRequestToken = data.oauth_token;
+						OAuthTokenSecret = data.oauth_token_secret;
+					} else {
+						// error
+						self.error(data);
+					}
+				})
+				.error(function(data) {
+					self.error(data);
+				});
+		},
+		// Step 2
+		authenticate: function() {
+			window.location = apiPath + 'oauth/authenticate?oauth_token=' + OAuthRequestToken;
+			// this should be done as a HTTP 302 redirect
+		},
+		// Step 3
+		verifyCallback: function(oauth_token, oauth_verifier) {
+			// verify token matches the one obtained in Step 1
+			if (oauth_token===OAuthRequestToken) {
+				// success
+				OAuthRequestVerifier = oauth_verifier;
+				this.generateAccessToken();
+			} else {
+				// error
+				self.error('callback verification failed');
+			}
+		},
+		// Step 4
+		generateAccessToken: function() {
+			$http.post(apiPath + 'oauth/access_token', { oauth_verifier: OAuthRequestVerifier })
+				.success(function(data) {
+					OAuthRequestToken = data.oauth_token;
+					OAuthTokenSecret = data.oauth_token_secret;
+					screenName = data.screen_name;
+					userId = data.user_id;
+					self.complete();
+				})
+				.error(function(data) {
+					self.error(data);
+				});
+		},
+		// Step 5
+		complete: function() {
+			console.log('Signin Complete -> ', OAuthRequestToken, OAuthTokenSecret, screenName, user_id);
+		},
+		error: function(data) {
+			console.log('error -> ', data);
+		}
+
+	};
+
+});
+

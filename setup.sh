044fae4a022b4280a44fea8a751ff0484b79f2c7
Andy Shora <andyshora@gmail.com>
2013-08-03 10:31:05 +0100

    Setup updated

--- setup.sh
+++ setup.sh
@@ -4,22 +4,26 @@ find . -type f -name "*.js" -print0 | xargs -0 sed -i '.bak' -e "s/newappApp/$ap
 find . -type f -name "*.html" -print0 | xargs -0 sed -i '.bak' -e "s/newappApp/$appName/g"
 echo  "Replaced app name references"
 cd app
-echo  "Installing npm dependencies"
-npm i
-echo  "Installing bower dependencies"
-bower i
-echo  "Done. You can now run 'grunt server' to see your app."
+echo  "Do you want to install npm and bower dependencies now? (y/n)"
+read install
+if [ "$install" == "y" ]; then
+	echo  "Installing npm dependencies"
+	npm i
+	echo  "Installing bower dependencies"
+	bower i
+	echo  "Done. You can now run 'grunt server' to see your app."
+fi
 echo  "Do you want to add a dev domain to use locally? (y/n)"
 read dev
 if [ "$dev" == "y" ]; then
 	echo "Enter dev domain, e.g. dev.myapp.com"
 	read domain
 	find . -type f -name "Gruntfile.js" -print0 | xargs -0 sed -i '.bak' -e "s/localhost/$domain/g"
+	echo  "Do you want to add this new dev domain to your hosts file? (y/n)"
+	read add
+	if [ "$add" == "y" ]; then
+		cp /etc/hosts ~/hosts.bak
+		sudo bash -c 'sudo echo "127.0.0.1	$domain" >> /etc/hosts'
+	fi
+	echo "Done."
 fi
-echo  "Do you want to add this new dev domain to your hosts file? (y/n)"
-read add
-if [ "$add" == "y" ]; then
-	cp /etc/hosts ~/hosts.bak
-	sudo bash -c 'sudo echo "127.0.0.1	$domain" >> /etc/hosts'
-fi
-echo "Done."

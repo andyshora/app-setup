4477c99fa839e686979742d8ee2a0ba51d798368
Andy Shora <andyshora@gmail.com>
2013-08-03 14:55:13 +0100

    OAuth token stored in localStorage

--- app/app/scripts/controllers/twitterLogin.js
+++ app/app/scripts/controllers/twitterLogin.js
@@ -4,12 +4,16 @@ angular.module('newappApp')
 	.controller('TwitterLoginCtrl', function ($scope, $http, $location) {
 		$scope.authState = 'checking-login';
 
+		var storedToken = localStorage.getItem('token');
+		$scope.loginQueryString = storedToken ? '?access_token=' + storedToken : '';
+
 		var getUserData = function() {
 			$http.get('api/index.php/auth/get_user?' + Math.random())
 				.success(function(data) {
 					if (data.uid) {
 						console.log('User data: ', data);
 						$scope.authState = 'logged-in';
+						localStorage.setItem('token', data.oauth_token);
 					} else {
 						// error
 						console.log('Error getting user data');

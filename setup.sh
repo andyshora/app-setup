e166e571c5510212df4cfc4732f8b0d14353278e
Andy Shora <andyshora@gmail.com>
2013-08-03 10:23:53 +0100

    setup updated

--- setup.sh
+++ setup.sh
@@ -9,3 +9,12 @@ npm i
 echo  "Installing bower dependencies"
 bower i
 echo  "Done. You can now run 'grunt server' to see your app."
+echo  "Do you want to add a dev domain to use locally on MAMP?"
+read dev
+if [ "$dev" == "y" ]; then
+	echo "Enter dev domain, e.g. dev.myapp.com"
+	read domain
+	find . -type f -name "Gruntfile.js" -print0 | xargs -0 sed -i '.bak' -e "s/localhost/$domain/g"
+	cp /etc/hosts ~/hosts.bak
+	sudo bash -c 'sudo echo "127.0.0.1	$domain" >> /etc/hosts'
+fi
